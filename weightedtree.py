00740026a9c9a912c59eb1156b64e259da712cb1
djo <declan.l.johnson@gmail.com>
2018-01-14 14:00:15 -0600

    Change output, calculate, add propNoArgs

--- weightedtree.py
+++ weightedtree.py
@@ -9,8 +9,8 @@ import minpy.numpy as np
 ORDER = 3
 
 
-inputLayers = []
-outputLayers = []
+inputLayers = [0]
+outputLayers = [2]
 
 Nodes = {}
 GlobalMEM = np.zeros(1024)
@@ -44,12 +44,6 @@ def fullLink(depth1, depth2):
     fullL2RLink(depth1, depth2)
     fullR2LLink(depth1, depth2)
 
-def addInput(d):
-    inputLayers.append(d)
-
-def addOutput(d):
-    outputLayers.append(d)
-
 def printOutput(gpun=0):
     for num in outputLayers:
         for node in Nodes[num]:
@@ -65,36 +59,42 @@ def calculate(nodeParents, node):
         GlobalMEM[1] += 1
 
     # step 2 calculate output based on input average (maybe other in future?)
-    
 
-    out = np.multiply(np.divide(GlobalMEM[0], GlobalMEM[1]), node.weight)
+    out = np.add(np.multiply(np.divide(GlobalMEM[0], GlobalMEM[1]), node.weight), node.bias)
+    if (out > 1):
+        out = 1.0
+    if (out < 0):
+        out = 0.0
     return out
 
 # must be same shape as inputs, condensed l2r
 def prop(startingInput):
-    print("start prop")
-    for noden in Nodes[0]:
-        noden.output = 0.5
+    for num in inputLayers:
+        for node in Nodes[num]:
+            node.output = startingInput[num][node.height]
 
     for i in range(1, len(Nodes)):
-        print("prop layer", i, len(Nodes[i]))
+        for node in Nodes[i]:
+            node.output = calculate(node.parents, node)
+
+def propNoArgs():
+    for i in range(0, len(Nodes)):
         for node in Nodes[i]:
             node.output = calculate(node.parents, node)
 
 
+
 nodeRange(0, 4)
 nodeRange(1, 4)
-nodeRange(2, 2)
-addInput(0)
-addOutput(2)
+nodeRange(2, 4)
 fullR2LLink(0,1)
-fullLink(1,2)
+fullR2LLink(1,2)
+fullR2LLink(2,0)
 printOutput()
-
-for i in range(0, 10):
+for i in range(0,10):
     with(gpu()):
-        prop([np.random.rand()]*4)
-    printOutput()
-    time.sleep(1)
+        propNoArgs()
+        printOutput()
+
 
 
